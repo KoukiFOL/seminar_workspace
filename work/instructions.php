68544108c02f6fa9544a04ae2b59588f63dda735
Kouki_FOL <kohki_ueyama@yahoo.co.jp>
2023-12-06 19:32:20 +0900

    ぐちゃぐちゃのままです。

--- work/instructions.php
+++ work/instructions.php
@@ -1,10 +1,4 @@
 <?php
-//controller
-require("../frames/urlpointer2.php");
-require("../frames/header.php");
-?>
-
-<?php
 if(isset($_GET['work'])){
     $work = $_GET['work'];
 }
@@ -13,12 +7,21 @@ if(isset($_GET['instruction'])){
     $instruction = $_GET['instruction'];
 }
 
-require("../../work/{$work}/filepointer.php");
 ?>
+
+<?php
+//controller
+require("../frames/urlpointer2.php");
+require("../frames/header.php");
+require("../work/{$work}/filepointer.php");
+?>
+
+
+
+
 <!-- view -->
 <html>
     <body>
-        <h2><?php $key ?></h2>
-
+        <h2><?php echo(array_search($instruction, $filelist)) ?></h2>
     </body>
 </html>
