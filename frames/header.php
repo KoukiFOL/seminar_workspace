1f49c3b177db92f1f7609d965e1ca49c5860502b
Kouki_FOL <kohki_ueyama@yahoo.co.jp>
2023-12-08 13:50:03 +0900

    見た目修正

--- frames/header.php
+++ frames/header.php
@@ -12,10 +12,10 @@
     </head>
     <body>
         <div id="header">
-            <h1 class="title"><a href="<?php echo($home);?>">久保ゼミ Ω班プレゼンテーション</a></h1>
+            <h1 class="title"><a href="<?php echo($home);?>">久保ゼミ Ω班</a></h1>
         </div>
         <ul class="navi">
-            <li><a href="<?php echo($home);?>">ホーム</a></li>
+            <li><a href="<?php echo($home);?>">HOME</a></li>
             <li><a href="<?php echo($work1);?>">Work1</a></li>
             <li><a href="<?php echo($work2);?>">Work2</a></li>
             <li><a href="<?php echo($work3);?>">Work3</a></li>
